b718945afa2cd3bceccc0e05198801ed13be844f
Yana Lepska <lepskayana1624@gmail.com>
2024-03-21 01:02:01 +0200

    To Up

--- main.js
+++ main.js
@@ -45,3 +45,33 @@ function onFormSubmit (event) {
     console.log(userData);
     form.reset();
 }
+
+function scrollToUp() {
+  const btn = document.querySelector('.btn-ring');
+
+  function showButton() {
+    btn.classList.remove('btn-ring-hidden');
+  }
+
+  function hideButton() {
+    btn.classList.add('btn-ring-hidden');
+  }
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 250 || document.documentElement.scrollTop > 250) {
+      showButton();
+    } else {
+      hideButton();
+    }
+  });
+
+  btn.onclick = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  };
+}
+
+scrollToUp();
